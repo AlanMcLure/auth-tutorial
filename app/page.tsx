975e4a1c81dec39b161a36995399c48442b29b02
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-01-15 23:59:18 +0100

    / cambiado

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main>
       <p className="font-semibold text-green-500">Hello Auth!</p>
-      <Button size="lg" variant="outline">Click me</Button>
     </main>
   )
 }
